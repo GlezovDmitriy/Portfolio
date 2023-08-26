89ab066dcddfa5ec3b30cd742ee9b4f5d18fbfb9
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-08-27 01:37:05 +0300

    added Skill section

---
+++ src/layout/sections/skills/skills.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from "styled-components";
+import SectionTitle from '../../../components/SectionTitle';
+import {FlexWrapper} from "../../../components/FlexWrapper";
+
+export const Skills = () => {
+    return (
+        <StyledSkills>
+<SectionTitle>My skills</SectionTitle>
+       <FlexWrapper>
+
+       </FlexWrapper>
+        </StyledSkills>
+    );
+};
+
+const StyledSkills = styled.section`
+
+`
+export default Skills;
