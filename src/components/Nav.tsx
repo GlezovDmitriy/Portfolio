bbee51977fa012d13f5762ac584543f5e7732bdb
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-08-26 20:50:49 +0300

    added fales Logo and Nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Skills</a>
+                    </li>
+                    <li>
+                        <a href="">Works</a>
+                    </li>
+                    <li>
+                        <a href="">Testimony</a>
+                    </li>
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Nav;
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,37 +1,19 @@
 import React from 'react';
 import styled from "styled-components";
+import Logo from "../../components/Logo";
+import Nav from "../../components/Nav";
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <a href="">
-                <img src="" alt=""/>
-            </a>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="">Home</a>
-                    </li>
-                    <li>
-                        <a href="">Skills</a>
-                    </li>
-                    <li>
-                        <a href="">Works</a>
-                    </li>
-                    <li>
-                        <a href="">Testimony</a>
-                    </li>
-                    <li>
-                        <a href="">Contact</a>
-                    </li>
-                </ul>
-            </nav>
+            <Logo></Logo>
+            <Nav></Nav>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-background-color: darksalmon;
+  background-color: darksalmon;
 `
-const Styled
+
 export default Header;
