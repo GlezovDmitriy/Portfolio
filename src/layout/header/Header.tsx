08f842b56e03acbe4fc5d187c564bfa761a38e72
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-08-26 20:42:19 +0300

    added directory components

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from "styled-components";
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <a href="">
+                <img src="" alt=""/>
+            </a>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Skills</a>
+                    </li>
+                    <li>
+                        <a href="">Works</a>
+                    </li>
+                    <li>
+                        <a href="">Testimony</a>
+                    </li>
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </StyledHeader>
+    );
+};
+
+const StyledHeader = styled.header`
+background-color: darksalmon;
+`
+const Styled
+export default Header;
