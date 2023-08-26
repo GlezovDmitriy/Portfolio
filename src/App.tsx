478bb05c27f02807390a8131e6f7ca31e997290b
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-08-26 20:42:19 +0300

    add new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import styled from 'styled-components';
+import Header from "./layout/header/Header";
 
 
 function App() {
     return (
         <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
+            <Header/>
         </div>
     );
 }
@@ -13,7 +14,7 @@ function App() {
 export default App;
 
 const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
+  font-size: 1.5em;
+  text-align: center;
+  color: #e91e63;
 `;
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styled from "styled-components";
+import Logo from "../../components/Logo";
+import Nav from "../../components/Nav";
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Logo></Logo>
+            <Nav></Nav>
+        </StyledHeader>
+    );
+};
+
+const StyledHeader = styled.header`
+  background-color: darksalmon;
+`
+
+export default Header;
