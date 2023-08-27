723cb03449495a7951b780f74480e8848e5ed886
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-08-27 21:04:35 +0300

    changed Skill section

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
-const SectionTitle = styled.h2`
+export const SectionTitle = styled.h2`
 color: red;
+  text-align: center;
 `
--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -2,18 +2,30 @@ import React from 'react';
 import styled from "styled-components";
 import Icon from "../../../../components/icon/Icon";
 
-const Skill = () => {
+
+type SkillPropsType = {
+    iconId: string
+    title: string
+    description: string
+}
+export const Skill = (props: SkillPropsType) => {
     return (
-        <StyledSkill>
-            <Icon iconId={"html"}/>
-            <SkillTitle>html5</SkillTitle>
-            <SkillText>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim</SkillText>
+        <StyledSkill >
+            <Icon iconId={props.iconId}/>
+            <SkillTitle>{props.title}</SkillTitle>
+            <SkillText>{props.description}</SkillText>
         </StyledSkill>
     );
 };
 
 const StyledSkill = styled.div`
-
+width: 30%;
+  text-align: center;
+  background-color: #f3e3c9;
+  margin: 5px;
+  border-radius: 5px;
+  border: 1px solid gray;
+  box-shadow: 2px 2px gray;
 `
 
 const SkillTitle = styled.h3`
