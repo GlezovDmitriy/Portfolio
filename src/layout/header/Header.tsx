fdb26a172fb8b59d89a0c6a3e710d71cf59ebc2c
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-09-04 19:12:19 +0300

    changed HeaderMenu

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import styled from "styled-components";
 import Logo from "../../components/Logo";
-import Menu from "../../components/Menu";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {HederMenu} from "./hederMenu/HederMenu";
 
 
 const items =["Home","Skills", "Works", "Testimony", "Contact"]
@@ -14,7 +14,7 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify={"space-between"} align={"center"}>
                     <Logo></Logo>
-                    <Menu menuItems={items}></Menu>
+                    <HederMenu menuItems={items}></HederMenu>
                 </FlexWrapper>
             </Container>
         </StyledHeader>
@@ -22,7 +22,14 @@ export const Header = () => {
 };
 
 const StyledHeader = styled.header`
-  background-color: darksalmon;
+  background-color: rgba(31, 31, 32, 0.9);
+  padding: 20px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 99999;
+  
   
 `
 
