f4a53a3bb9d1a82f1b3c3fe7350ac5d5a5682369
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-09-05 23:01:08 +0300

    changed Testimony section

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,13 +4,16 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import Icon from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {IconWrapper} from "../skills/skill/Skill";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
             <SectionTitle>Testimony</SectionTitle>
             <FlexWrapper direction={"column"} align={"center"}>
-                <Icon iconId={"html"}/>
+                <IconWrapper>
+                    <Icon iconId={"html"}/>
+                </IconWrapper>
                 <Slider/>
             </FlexWrapper>
 
@@ -21,5 +24,8 @@ export const Testimony = () => {
 const StyledTestimony = styled.section`
 background-color: #e4bcf3;
 min-height: 50vh;
-align-items: center;
+
+  ${IconWrapper} {
+  margin: 40px 0 72px;
+}
 `
